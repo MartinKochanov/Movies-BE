a2594c199d8fe47f72849151dc62c761ab9eaa8d
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2025-04-03 01:59:09 +0300

    Test containers configured. Create movie member method tested in all possible scenarios.

--- src/test/java/com/mk/movies/web/MovieCrewMemberControllerTest.java
+++ src/test/java/com/mk/movies/web/MovieCrewMemberControllerTest.java
@@ -1,9 +1,14 @@
 package com.mk.movies.web;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.mk.movies.domain.movie_crew_member.document.MovieCrewMember;
 import com.mk.movies.domain.movie_crew_member.dto.MovieCrewMemberRequest;
+import com.mk.movies.domain.movie_crew_member.repository.MovieCrewMemberRepository;
+import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -15,6 +20,7 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+//TODO: Prevent file upload in tests
 @SpringBootTest
 @Testcontainers
 @AutoConfigureMockMvc
@@ -22,12 +28,17 @@ class MovieCrewMemberControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private MovieCrewMemberRepository movieCrewMemberRepository;
 
     private static final String BASE_URL = "/movie-crew-members";
+    private static MovieCrewMember movieCrewMember;
     private static MovieCrewMemberRequest movieCrewMemberRequest;
 
     @BeforeEach
     void setUp() {
+        movieCrewMemberRepository.deleteAll();
+
         var mockImage = new MockMultipartFile(
             "image",
             "image.jpg",
@@ -40,6 +51,14 @@ class MovieCrewMemberControllerTest {
             "Last Name",
             mockImage
         );
+
+        movieCrewMember = new MovieCrewMember();
+        movieCrewMember.setFirstName("First Name");
+        movieCrewMember.setLastName("Last Name");
+        movieCrewMember.setImageUrl("http://localhost:8080/image.jpg");
+
+        movieCrewMemberRepository.save(movieCrewMember);
+
     }
 
     @Test
@@ -73,5 +92,36 @@ class MovieCrewMemberControllerTest {
                 .param("firstName", movieCrewMemberRequest.firstName())
                 .param("lastName", movieCrewMemberRequest.lastName()))
             .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getAll_returns_ok() throws Exception {
+        mockMvc.perform(get(BASE_URL))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.content").isArray())
+            .andExpect(jsonPath("$.content[0].firstName").value(movieCrewMember.getFirstName()))
+            .andExpect(jsonPath("$.content[0].lastName").value(movieCrewMember.getLastName()))
+            .andExpect(jsonPath("$.content[0].imageUrl").value(movieCrewMember.getImageUrl()));
+    }
+
+    @Test
+    void getById_returns_ok_givenValidId() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/" + movieCrewMember.getId()))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.firstName").value(movieCrewMember.getFirstName()))
+            .andExpect(jsonPath("$.lastName").value(movieCrewMember.getLastName()))
+            .andExpect(jsonPath("$.imageUrl").value(movieCrewMember.getImageUrl()));
+    }
+
+    @Test
+    void getById_returns_bad_request_givenInvalidId() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/invalid-id"))
+            .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void getById_returns_not_found_givenNonExistentId() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/" + new ObjectId().toHexString()))
+            .andExpect(status().isNotFound());
     }
 }
