07ba70c47dfe07753fee176016d8bfdc5bd471c6
MartinKochanov <114015998+MartinKochanov@users.noreply.github.com>
2025-05-16 16:15:17 +0300

    add cors config

---
+++ src/main/java/com/mk/movies/infrastructure/config/CorsConfig.java
@@ -0,0 +1,25 @@
+package com.mk.movies.infrastructure.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        var config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
--- src/main/java/com/mk/movies/infrastructure/minio/MinioUtil.java
+++ src/main/java/com/mk/movies/infrastructure/minio/MinioUtil.java
@@ -4,18 +4,27 @@ public class MinioUtil {
 
     public static String getPublicReadPolicy(String bucketName) {
         return """
-            {
-                "Version": "2012-10-17",
-                "Statement": [
-                    {
-                        "Effect": "Allow",
-                        "Principal": "*",
-                        "Action": "s3:GetObject",
-                        "Resource": "arn:aws:s3:::%s/*"
-                    }
-                ]
-            }
-            """.formatted(bucketName);
+        {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": "*",
+                    "Action": "s3:GetObject",
+                    "Resource": "arn:aws:s3:::%s/*"
+                }
+            ],
+            "CORSRules": [
+                {
+                    "AllowedOrigins": ["http://localhost:5173"],
+                    "AllowedMethods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+                    "AllowedHeaders": ["*"],
+                    "ExposeHeaders": ["ETag", "x-amz-meta-custom-header"],
+                    "MaxAgeSeconds": 3000
+                }
+            ]
+        }
+        """.formatted(bucketName);
     }
 
     public static String extractFileName(String fileUrl) {
